41b7bfc856c26f61091d9b017b8c650d9765e461
samarjit <samarjitbravo@gmail.com>
2024-02-21 17:20:53 +0530

    create user routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // /users
+export class UsersController {
+  @Get() // GET /users
+  findAll() {
+    return [];
+  }
+
+  @Get(':id') //GET /user/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') //PATCH /user/:id
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') //DELETE /user/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
