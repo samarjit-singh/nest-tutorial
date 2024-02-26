15c0092b5c24b282fcc68237d65675b6e48f33f3
samarjit <samarjitbravo@gmail.com>
2024-02-26 17:05:13 +0530

    extend eployee service modal

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -7,22 +7,44 @@ export class EmployeeService {
   constructor(private readonly databaseService: DatabaseService) {}
 
   async create(createEmployeeDto: Prisma.EmployeeCreateInput) {
-    return 'This action adds a new employee';
+    return this.databaseService.employee.create({
+      data: createEmployeeDto
+    });
   }
 
-  async findAll() {
-    return `This action returns all employee`;
+  async findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) return this.databaseService.employee.findMany;
+    ({
+      where: {
+        role
+      }
+    });
+
+    return this.databaseService.employee.findMany();
   }
 
   async findOne(id: number) {
-    return `This action returns a #${id} employee`;
+    return this.databaseService.employee.findUnique({
+      where: {
+        id
+      }
+    });
   }
 
   async update(id: number, updateEmployeeDto: Prisma.EmployeeUpdateInput) {
-    return `This action updates a #${id} employee`;
+    return this.databaseService.employee.update({
+      where: {
+        id
+      },
+      data: updateEmployeeDto
+    });
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} employee`;
+    return this.databaseService.employee.delete({
+      where: {
+        id
+      }
+    });
   }
 }
